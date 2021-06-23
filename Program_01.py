518208d7e71c740d804c69fafba56055600a005c
YashVardhan-Act <84968831+YashVardhan-Act@users.noreply.github.com>
2021-06-23 22:12:43 +0530

    Add files via upload

---
+++ Program_01.py
@@ -0,0 +1,10 @@
+# This Program Calculates Area And Perimeter Of Rectangle
+
+l = int(input("Enter The Length: "))
+b = int(input("Enter The Breadth: "))
+
+area = l*b
+perimeter = 2*(l+b)
+
+print("The Area Of the Rectangle Is:", area)
+print("The Perimeter Of The Rectangle Is:", perimeter)
